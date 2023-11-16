3a787764170d064804fc5f3c2f7c3afd5635fdc7
JuanSamuelArbelaez <109239223+JuanSamuelArbelaez@users.noreply.github.com>
2023-11-16 00:16:56 -0500

    Mist-y -v0.1
    mist-y as GUI

--- BIMBIMBAMBAM/user_management.py
+++ BIMBIMBAMBAM/user_management.py
@@ -7,7 +7,7 @@ class UserManager:
         self.db = mysql.connector.connect(
             host="localhost",
             user="root",
-            password="root",
+            password="password",
             database="brainbox"
         )
         self.cursor = self.db.cursor(buffered=True)
