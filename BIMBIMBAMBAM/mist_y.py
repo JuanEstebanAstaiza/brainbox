5c818da623cb22cbc6c0c3198f6d579f2752ac20
Juan Astaiza <juanastaizafuenmayor@gmail.com>
2023-11-16 09:43:13 -0500

    Big forms

--- BIMBIMBAMBAM/mist_y.py
+++ BIMBIMBAMBAM/mist_y.py
@@ -1,19 +1,32 @@
 #GUI
 import tkinter as tk
 from tkinter import messagebox
+from user_management import UserManager
 
 def create_account():
     top = tk.Toplevel(root)
     top.title("Create Account")
-    top.geometry("300x200")
+    top.geometry("500x200")
 
-    tk.Label(top, text="Email:").pack()
+    tk.Label(top, text="Username:").pack()
     tk.Entry(top).pack()
 
     tk.Label(top, text="Password:").pack()
     tk.Entry(top, show="*").pack()
 
-    tk.Button(top, text="Create Account", command=top.destroy).pack()
+    tk.Label(top, text="UserType:").pack()
+    tk.Entry(top).pack()
+
+
+    def registro():
+
+        user_manager = UserManager()
+       # user_manager.register_user(email.entry.get(),password.entry.get(),Usertype.entry.get())
+        messagebox.showinfo("Account Created","Account Successfully created!")
+        top.destroy()
+    tk.Button(top, text="Create Account", command=registro).pack()
+
+
 
 def forgot_password():
     messagebox.showinfo("Forgot Password", "Please contact the system administrator to reset your password.")
